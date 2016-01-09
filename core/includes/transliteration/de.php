6a49bb41fe794ba57216b75cbddd66adef12c045
Jerome Danthinne <jerome@grincheux.be>
2016-01-09 10:24:43 +0100

    Added language-specific overrides for transliteration (D8 backport)

---
+++ core/includes/transliteration/de.php
@@ -0,0 +1,10 @@
+<?php
+
+$overrides['de'] = array(
+  0xC4 => 'Ae',
+  0xD6 => 'Oe',
+  0xDC => 'Ue',
+  0xE4 => 'ae',
+  0xF6 => 'oe',
+  0xFC => 'ue',
+);
---
+++ core/includes/transliteration/dk.php
@@ -0,0 +1,8 @@
+<?php
+
+$overrides['dk'] = array(
+  0xC5 => 'Aa',
+  0xD8 => 'Oe',
+  0xE5 => 'aa',
+  0xF8 => 'oe',
+);
